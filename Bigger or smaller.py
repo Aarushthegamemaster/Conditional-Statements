68df286932924b194028b0f444f4b7d4d87bf4bd
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-03-02 17:42:37 -0800

    Bigger_or_smaller

---
+++ Bigger or smaller.py
@@ -0,0 +1,8 @@
+i = int(input("please enter a number here = "))
+if (i < 15):
+    print(i,"is smaller than 15")
+    print("i'm in if block")
+else:
+    print(i,"is is bigger than 15")
+    print("i'm in else block")
+print("im not in if or else block")
